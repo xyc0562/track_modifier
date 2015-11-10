ec32f485ea77ee2fbf51a11e081a48740464119e
Yecheng Xu <xyc0562@gmail.com>
2015-11-10 15:38:09 +0800

    Bugfix

--- lib/track_modifier.rb
+++ lib/track_modifier.rb
@@ -3,6 +3,7 @@ require 'active_support/concern'
 require 'active_record'
 require 'track_modifier/base'
 require 'track_modifier/migration'
+require 'track_modifier/config'
 
 module TrackModifier
   ActiveRecord::Base.send :include, TrackModifier::Base
